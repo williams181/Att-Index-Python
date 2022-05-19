5d17b4b05229d47bcf947752dc3f1ff64086c475
William Roberto Andrade da silva <31158332+williams181@users.noreply.github.com>
2022-05-19 08:36:55 -0300

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,15 +1,2 @@
 create database myflask;
 use myflask;
-
-CREATE TABLE funcionario (
-  id int NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(50),
-  email VARCHAR(50),
-  PRIMARY KEY (id)
-);
-
-INSERT INTO funcionario
-  (nome, email)
-VALUES
-  ('william', 'william@gmail.com'),
-  ('vagner', 'vagner@hotmail.com');
